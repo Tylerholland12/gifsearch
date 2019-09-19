5b58556edf0489a00d0b54efecb782352cccda30
Tyler_Holland <tyler.holland@students.makeschool.com>
2019-09-19 14:57:42 -0700

    added all files and updated comments

--- app.py
+++ app.py
@@ -46,29 +46,6 @@ def index():
     return render_template("index.html", gifs=gifs)
 
 
-# class AppTests(unittest.TestCase): 
-
-#     # This runs implicitly before any tests are run
-#     # We use this to set up our app before we test on it
-#     def setUp(self):
-#         # creates a test client
-#         self.app = app.test_client()
-#         # propagate the exceptions to the test client
-#         self.app.testing = True
-
-#     def test_home_status_code(self):
-#         # sends HTTP GET request to the application
-#         # on the specified path
-#         result = self.app.get('/') 
-
-#         self.assertIn(b'Hello', result.data)
-
-#         # assert the status code of the response
-#         self.assertEqual(result.status_code, 200) 
-
-
-# if __name__ == '__main__':
-#     unittest.main()
 
 if __name__ == '__main__':
     app.run(debug=True)
